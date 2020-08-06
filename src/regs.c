50c97756db651aa01d7a08d30d449dcf264e92a6
Roey Farkash <roeyfarkash@gmail.com>
2020-08-06 23:36:27 +0300

    better reg printing

--- src/regs.c
+++ src/regs.c
@@ -49,10 +49,11 @@ status print_all_regs(pid_t pid){
     if (-1 == ptrace(PTRACE_GETREGS, pid, NULL, &regs)) {
         return ERROR;
     }
-    printf("r15=0x%llx\nr14=0x%llx\nr13=0x%llx\nr12=0x%llx\nrbp=0x%llx\nrbx=0x%llx\nr11=0x%llx\nr10=0x%llx\nr9=0x%llx\nr8=0x%llx\nrax=0x%llx\nrcx=0x%llx\nrdx=0x%llx\nrsi=0x%llx\nrdi=0x%llx\norig_rax=0x%llx\nrip=0x%llx\ncs=0x%llx\neflags=0x%llx\nrsp=0x%llx\nss=0x%llx\nfs_base=0x%llx\ngs_base=0x%llx\nds=0x%llx\nes=0x%llx\nfs=0x%llx\ngs=0x%llx\n",
-            regs.r15, regs.r14, regs.r13, regs.r12, regs.rbp, regs.rbx, regs.r11, regs.r10, regs.r9, regs.r8,
-             regs.rax, regs.rcx, regs.rdx, regs.rsi, regs.rdi, regs.orig_rax, regs.rip, regs.cs, regs.eflags,
-              regs.rsp, regs.ss, regs.fs_base, regs.gs_base, regs.ds, regs.es, regs.fs, regs.gs);
+
+    for (int i=0; i < reg_converter_length; i++) {
+        printf("%s=0x%llx\n", reg_converter[i].reg_name, *(reg_converter[i].value));
+    }
+
     return NO_ERROR;
 }
 
